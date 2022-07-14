3ab471b0d41f27dc9c37ca0ee1a74f754425516b
yoshi <yoshi.yoon@gmail.com>
2022-07-14 03:07:57 +0000

    update script

--- scripts/create_o7k_octavia_handson_resources.sh
+++ scripts/create_o7k_octavia_handson_resources.sh
@@ -5,7 +5,7 @@ set -x
 
 # Create a client instance
 openstack server create \
-  --image auto-sync/ubuntu-focal-20.04-amd64-server-20220615-disk1.img \
+  --image auto-sync/ubuntu-focal-20.04-amd64-server-20220711-disk1.img \
   --flavor m1.small \
   --network internal-network \
   --security-group ssh \
@@ -16,7 +16,7 @@ openstack server create \
 
 # Create two test backend instances
 openstack server create \
-  --image auto-sync/ubuntu-focal-20.04-amd64-server-20220615-disk1.img \
+  --image auto-sync/ubuntu-focal-20.04-amd64-server-20220711-disk1.img \
   --flavor m1.small \
   --network internal-network \
   --security-group ssh \
